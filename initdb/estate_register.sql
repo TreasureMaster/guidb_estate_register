c926039a3dfec0b6d066712dc60299625aaf463d
Alex <54545559+TreasureMaster@users.noreply.github.com>
2022-05-16 17:14:55 +0300

    создание БД

--- initdb/estate_register.sql
+++ initdb/estate_register.sql
@@ -1,99 +1,135 @@
--- Таблица арендатора (customer)
-CREATE TABLE IF NOT EXISTS public.customer
+-- Таблица материала стен (materials)
+CREATE TABLE IF NOT EXISTS public.materials
 (
-    "IDCustomer" serial NOT NULL,
-    "INN" BIGINT NOT NULL,
-    "Status" VARCHAR(20) NOT NULL,
-    "Customer" VARCHAR(60) NOT NULL,
-    "AddressCust" VARCHAR(250) NOT NULL,
-    "Bank" VARCHAR(60) NOT NULL,
-    "Account" VARCHAR(20) NOT NULL,
-    "Tax" VARCHAR(60) NOT NULL,
-    "Chief" VARCHAR(40) NOT NULL,
-    "Phone" VARCHAR(20) NOT NULL,
-    CONSTRAINT customer_pkey PRIMARY KEY ("IDCustomer"),
-    CONSTRAINT inn_unique UNIQUE ("INN")
+    "IDMaterial" serial NOT NULL,
+    "Material" VARCHAR(60) NOT NULL,
+    CONSTRAINT material_pkey PRIMARY KEY ("IDMaterial")
 );
 
-ALTER TABLE public.customer
+ALTER TABLE public.materials
     OWNER to postgres;
 
--- Таблица ответственных от агенства (employees)
-CREATE TABLE IF NOT EXISTS public.employees
+-- Таблица зданий (buildings)
+CREATE TABLE IF NOT EXISTS public.buildings
 (
-    "IDEmployee" serial NOT NULL,
-    "Employee" VARCHAR(60) NOT NULL,
-    CONSTRAINT employee_pkey PRIMARY KEY ("IDEmployee")
-);
-
-ALTER TABLE public.employees
-    OWNER to postgres;
-
--- Таблица периодов оплаты
-CREATE TABLE IF NOT EXISTS public.periods
-(
-    "IDPeriod" serial NOT NULL,
-    "Period" VARCHAR(60) NOT NULL,
-    CONSTRAINT period_pkey PRIMARY KEY ("IDPeriod")
-);
-
-ALTER TABLE public.periods
-    OWNER to postgres;
-
--- Таблица рекламных щитов (billboard)
-CREATE TABLE IF NOT EXISTS public.billboard
-(
-    "IDBillboard" serial NOT NULL,
-    "Address" VARCHAR(60) NOT NULL,
-    "Orientation" VARCHAR(60) NOT NULL,
-    "Square" NUMERIC(10, 2) NOT NULL,
-    "District" VARCHAR(20) NOT NULL,
-    "Size" VARCHAR(20) NOT NULL,
+    "IDKadastr" serial NOT NULL,
+    "BuildingName" VARCHAR(60) NOT NULL,
+    "Land" NUMERIC(10, 2) NOT NULL,
+    "Address" VARCHAR(250) NOT NULL,
+    "Year" SMALLINT NOT NULL,
+    "Wear" SMALLINT NOT NULL,
+    "Flow" SMALLINT NOT NULL,
     "Picture" VARCHAR(250),
-    CONSTRAINT regnumber_pkey PRIMARY KEY ("IDBillboard")
-);
-
-ALTER TABLE public.billboard
-    OWNER to postgres;
-
--- Таблица договоров (treaty)
-CREATE TABLE IF NOT EXISTS public.treaty
-(
-    "IDTreaty" serial NOT NULL,
-    "DateStart" date NOT NULL,
-    "StopDate" date NOT NULL,
-    "SignDate" date NOT NULL,
-    "Advertisement" boolean NOT NULL,
-    "Cost" NUMERIC(10, 2) NOT NULL,
-    "Leasing" NUMERIC(10, 2) NOT NULL,
-    "PeriodID" INTEGER NOT NULL,
-    "EmployeeID" INTEGER NOT NULL,
-    "BillboardID" INTEGER NOT NULL,
-    "CustomerID" INTEGER NOT NULL,
-    CONSTRAINT treaty_pkey PRIMARY KEY ("IDTreaty"),
-    CONSTRAINT employee_fkey FOREIGN KEY ("EmployeeID")
-        REFERENCES public.employees ("IDEmployee") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    CONSTRAINT period_fkey FOREIGN KEY ("PeriodID")
-        REFERENCES public.periods ("IDPeriod") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    CONSTRAINT billboard_fkey FOREIGN KEY ("BillboardID")
-        REFERENCES public.billboard ("IDBillboard") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    CONSTRAINT customer_fkey FOREIGN KEY ("CustomerID")
-        REFERENCES public.customer ("IDCustomer") MATCH SIMPLE
+    "Comment" TEXT,
+    "MaterialID" INTEGER NOT NULL,
+    CONSTRAINT kadastr_pkey PRIMARY KEY ("IDKadastr"),
+    CONSTRAINT material_fkey FOREIGN KEY ("MaterialID")
+        REFERENCES public.materials ("IDMaterial") MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
 );
 
-ALTER TABLE public.treaty
+ALTER TABLE public.buildings
+    OWNER to postgres;
+
+-- Таблица назначения помещения (targets)
+CREATE TABLE IF NOT EXISTS public.targets
+(
+    "IDTarget" serial NOT NULL,
+    "Target" VARCHAR(60) NOT NULL,
+    CONSTRAINT target_pkey PRIMARY KEY ("IDTarget")
+);
+
+ALTER TABLE public.targets
+    OWNER to postgres;
+
+-- Таблица кафедр (departments)
+CREATE TABLE IF NOT EXISTS public.departments
+(
+    "IDDepartment" serial NOT NULL,
+    "DepartmentName" VARCHAR(60) NOT NULL,
+    "Boss" VARCHAR(60) NOT NULL,
+    "Phone" BIGINT NOT NULL,
+    "OfficeDean" VARCHAR(60) NOT NULL,
+    CONSTRAINT department_pkey PRIMARY KEY ("IDDepartment")
+);
+-- Зачем Здание и Ответственный ?
+
+ALTER TABLE public.departments
+    OWNER to postgres;
+
+-- Таблица помещений (halls)
+CREATE TABLE IF NOT EXISTS public.halls
+(
+    "IDHall" serial NOT NULL,
+    "HallNumber" SMALLINT NOT NULL,
+    "HallSquare" NUMERIC(10, 2) NOT NULL,
+    "Windows" SMALLINT NOT NULL,
+    "Heaters" SMALLINT NOT NULL,
+    "TargetID" INTEGER NOT NULL,
+    "DepartmentID" INTEGER NOT NULL,
+    "KadastrID" INTEGER NOT NULL,
+    CONSTRAINT halls_pkey PRIMARY KEY ("IDHall"),
+    CONSTRAINT target_fkey FOREIGN KEY ("TargetID")
+        REFERENCES public.targets ("IDTarget") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT department_fkey FOREIGN KEY ("DepartmentID")
+        REFERENCES public.departments ("IDDepartment") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT kadastr_fkey FOREIGN KEY ("KadastrID")
+        REFERENCES public.buildings ("IDKadastr") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
+
+ALTER TABLE public.halls
+    OWNER to postgres;
+
+-- Таблица ответственных (chiefs)
+CREATE TABLE IF NOT EXISTS public.chiefs
+(
+    "IDChief" serial NOT NULL,
+    "Chief" VARCHAR(60) NOT NULL,
+    "AddressChief" VARCHAR(120) NOT NULL,
+    "Experience" SMALLINT NOT NULL,
+    CONSTRAINT chiefs_pkey PRIMARY KEY ("IDChief")
+);
+
+ALTER TABLE public.chiefs
+    OWNER to postgres;
+
+-- Таблица имущества (units)
+CREATE TABLE IF NOT EXISTS public.units
+(
+    "IDUnit" serial NOT NULL,
+    "UnitName" VARCHAR(60) NOT NULL,
+    "DateStart" DATE NOT NULL,
+    "Cost" NUMERIC(10, 2) NOT NULL,
+    "CostYear" SMALLINT NOT NULL,
+    "CostAfter" NUMERIC(10, 2) NOT NULL,
+    "Period" VARCHAR(60) NOT NULL,
+    "HallID" INTEGER NOT NULL,
+    "ChiefID" INTEGER NOT NULL,
+    CONSTRAINT units_pkey PRIMARY KEY ("IDUnit"),
+    CONSTRAINT hall_fkey FOREIGN KEY ("HallID")
+        REFERENCES public.halls ("IDHall") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT chief_fkey FOREIGN KEY ("ChiefID")
+        REFERENCES public.chiefs ("IDChief") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
+
+ALTER TABLE public.units
     OWNER to postgres;
 
 -- Таблица пользователей (пароль - sha1)
@@ -107,44 +143,63 @@ CREATE TABLE public.users(
 ALTER TABLE public.users
     OWNER to postgres;
 
--- Арендаторы
-INSERT INTO public.customer
-("INN", "Status", "Customer", "AddressCust", "Bank", "Account", "Tax", "Chief", "Phone")
+-- Материал здания
+INSERT INTO public.materials ("Material")
 VALUES
-(1242005939, 'ООО', 'Дельта-Тест', 'г.Владивосток, ул.Никифорова, 2А', 'Примтеркомбанк', '40817810099910004312', 'МИФНС России №9 по Приморскому краю', 'Нигматуллин Руслан Насимович', '89141524687'),
-(1276069176, 'ООО', 'Транстехсервис', 'г.Владивосток, ул.Луговая, 59', 'Морской банк', '40817810099910004588', 'МИФНС России №12 по Приморскому краю', 'Романов Роман Анатольевич', '89241545223'),
-(1012002692, 'ОАО', 'Защита-Лес', 'г.Находка, ул.Пограничная, 6, 219', 'Банк Синара', '40817810099910008946', 'МИФНС России №15 по Приморскому краю', 'Гринберг Самоил Израилович', '89043874930');
+('Кирпич'),
+('Дерево'),
+('Железобетон'),
+('Пеноблок');
 
--- Отвественные от агенства
-INSERT INTO public.employees ("Employee")
+-- Здания
+INSERT INTO public.buildings
+("BuildingName", "Land", "Address", "Year", "Wear", "Flow", "Picture", "Comment", "MaterialID")
 VALUES
-('Иванов'),
-('Петров'),
-('Сидоров'),
-('Рогов');
+('Административное', 4000, 'ул.Никифорова, 2А', 1982, 50, 5, null, null, 1),
+('Корпус 1', 3000, 'ул.Луговая, 59', 1985, 20, 3, null, null, 3),
+('Корпус 2', 3200, 'ул.Пограничная, 6', 1986, 30, 3, null, null, 3);
 
--- Периоды оплаты
-INSERT INTO public.periods ("Period")
+-- Назначение помещения
+INSERT INTO public.targets ("Target")
 VALUES
-('ежемесячная'),
-('квартальная'),
-('годовая');
+('аудитория'),
+('лаборатория'),
+('вычислительный центр'),
+('деканат');
 
--- Рекламный щит
-INSERT INTO public.billboard
-("Address", "Orientation", "Square", "District", "Size", "Picture")
+-- Кафедра
+INSERT INTO public.departments
+("DepartmentName", "Boss", "Phone", "OfficeDean")
 VALUES
-('г.Владивосток, ул.Стрелковая, 46', 'ОДОРА', 18, 'Ленинский', '3x6', null),
-('г.Владивосток, ул.Снеговая, 32', 'ОДОРА', 18, 'Первореченский', '3x6', null),
-('г.Владивосток, ул.Героев-тихоокеанцев, 23А', 'Депо-2', 36, 'Первомайский', '3x12', null),
-('г.Владивосток, ул.Алеутская, 14', 'ОДОРА', 18, 'Фрунзенский', '3x6', null);
+('Философия', 'Ячин Сергей Евгеньевич', 842322652424, 'Гуманитарные науки'),
+('Социальные науки', 'Кузина Ирина Геннадьевна', 842322652424, 'Гуманитарные науки'),
+('Психология', 'Батурина Оксана Сергеевна', 842322652424, 'Гуманитарные науки'),
+('История и археология', 'Пахомов Олег Станиславович', 842322652424, 'Гуманитарные науки');
 
--- Договоры
-INSERT INTO public.treaty ("DateStart", "StopDate", "SignDate", "Advertisement", "Cost", "Leasing", "PeriodID", "EmployeeID", "BillboardID", "CustomerID")
+-- Помещения
+INSERT INTO public.halls
+("HallNumber", "HallSquare", "Windows", "Heaters", "TargetID", "DepartmentID", "KadastrID")
 VALUES
-('2021-03-02', '2021-04-27', '2021-03-01', true, 15000, 28000, 1, 1, 1, 1),
-('2021-05-19', '2021-06-19', '2021-05-02', true, 20000, 34000, 1, 2, 3, 2),
-('2021-06-20', '2021-12-31', '2021-06-08', false, 0, 12000, 2, 3, 4, 3);
+(101, 48, 5, 5, 1, 1, 2),
+(202, 44, 4, 4, 1, 2, 2),
+(122, 32, 3, 3, 2, 3, 3),
+(308, 46, 4, 4, 4, 4, 3);
+
+-- Здания
+INSERT INTO public.chiefs
+("Chief", "AddressChief", "Experience")
+VALUES
+('Иванов', 'ул.Никифорова, 56', 2),
+('Петров', 'ул.Луговая, 22', 3),
+('Кошкин', 'ул.Пограничная, 9', 3);
+
+-- Имущество
+INSERT INTO public.units
+("UnitName", "DateStart", "Cost", "CostYear", "CostAfter", "Period", "HallID", "ChiefID")
+VALUES
+('Шкаф', '2005-04-27', 5600, 2020, 4700, 30, 1, 1),
+('Обогреватель', '2016-06-19', 3200, 2021, 2800, 20, 2, 2),
+('Компьютер', '2019-12-31', 46000, 2022, 39500, 10, 4, 3);
 
 -- Пользователи
 INSERT INTO public.users ("Login", "Password", "is_admin")
